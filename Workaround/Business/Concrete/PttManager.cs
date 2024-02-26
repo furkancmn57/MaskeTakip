48518e2995a5042802ed590da6d849a197cf775a
Furkan Çimen <77547550+furkancmn57@users.noreply.github.com>
2024-02-27 00:21:58 +0300

    add: mask follow

---
+++ Workaround/Business/Concrete/PttManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstarct;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PttManager : ISupplierService
+{
+    private IApplicantService _applicantService;
+    public PttManager(IApplicantService applicantService)
+    {
+        _applicantService = applicantService;
+    }
+    public void GiveMask(Person person)
+    {
+        if (_applicantService.CheckPerson(person))
+        {
+            Console.WriteLine(person.FirstName + " isimli kişiye maske verildi.");
+        }
+        else
+        {
+            Console.WriteLine(person.FirstName + " isimli kişiye maske verilemedi.");
+        }
+    }
+}
---
+++ Workaround/Workaround/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            FirstName = "FURKAN",
+            LastName = "ÇİMEN",
+            DateOfBirthYear = 2003,
+            NotionalIdentity = 12345
+        };
+        PttManager pttManager = new PttManager(new PersonManager());
+
+        pttManager.GiveMask(person);
+
+    }
+}
